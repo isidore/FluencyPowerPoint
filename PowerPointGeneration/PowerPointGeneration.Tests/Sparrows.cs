090e2c8cc63bb8a7394264206ceed924244430c0
Llewellyn Falco <isidore@setgame.com>
2015-05-25 20:31:39 -0500

    Better timings and labels

--- PowerPointGeneration/PowerPointGeneration.Tests/Sparrows.cs
+++ PowerPointGeneration/PowerPointGeneration.Tests/Sparrows.cs
@@ -1,5 +1,6 @@
 ﻿//using System.Data;
 
+using System;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -51,56 +52,73 @@ namespace PowerPointGeneration.Tests
 				CustomLayout customLayout = pptPresentation.SlideMaster.CustomLayouts[PpSlideLayout.ppLayoutText];
 				CustomLayout textLayout = pptPresentation.SlideMaster.CustomLayouts[PpSlideLayout.ppLayoutTitle];
 
-				Slides slides;
-
-				slides = pptPresentation.Slides;
+				Slides slides = pptPresentation.Slides;
 				int counter = 0;
 				int page = 1;
 				foreach (var sparrow in SparrowData.Get())
 				{
 					counter++;
-
 					// Question
-					var slide = slides.AddSlide(page++, customLayout);
-					Shape shape = slide.Shapes[2];
-					string localFilename = @"c:\temp\birds\sparrow{0}.jpg".FormatWith(counter);
-					slide.Shapes.AddPicture(localFilename, MsoTriState.msoFalse, MsoTriState.msoTrue, shape.Left, shape.Top,
-						shape.Width, shape.Height);
-					float time = GetTimingsForImage(counter);
-					totalTime += time;
-					slide.SlideShowTransition.AdvanceTime = time;
-					slide.SlideShowTransition.AdvanceOnTime = MsoTriState.msoTrue;
-
+					totalTime = AddPicturePage(slides, page, customLayout, counter, totalTime);
+					page++;
 					// Answer
-					slide = slides.AddSlide(page++, textLayout);
-					var objText = slide.Shapes[1].TextFrame.TextRange;
-					objText.Text = sparrow.Item1;
-					objText.Font.Name = "Arial";
-					objText.Font.Size = 80;
-					time = GetTimingsForAnswer(counter);
-					totalTime += time;
-					slide.SlideShowTransition.AdvanceTime =time;
-					slide.SlideShowTransition.AdvanceOnTime = MsoTriState.msoTrue;
+					totalTime = AddAnswerPage(slides, page, textLayout, sparrow, counter, totalTime);
+					page++;
 				}
 				Logger.Variable("Total Time", "{0:00}:{0:00}".FormatWith(totalTime/60, totalTime %60));
 			}
 		}
 
+		private static float AddPicturePage(Slides slides, int page, CustomLayout customLayout, int counter, float totalTime)
+		{
+			var slide = slides.AddSlide(page, customLayout);
+			Shape shape = slide.Shapes[2];
+			string localFilename = @"c:\temp\birds\sparrow{0}.jpg".FormatWith(counter);
+			slide.Shapes.AddPicture(localFilename, MsoTriState.msoFalse, MsoTriState.msoTrue, shape.Left, shape.Top,
+				shape.Width, shape.Height);
+			float time = GetTimingsForImage(counter);
+			totalTime += time;
+			slide.SlideShowTransition.AdvanceTime = time;
+			slide.SlideShowTransition.AdvanceOnTime = MsoTriState.msoTrue;
+			return totalTime;
+		}
+
+		private static float AddAnswerPage(Slides slides, int page, CustomLayout textLayout, Tuple<string, string> sparrow, int counter,
+			float totalTime)
+		{
+			Slide slide;
+			float time;
+			slide = slides.AddSlide(page, textLayout);
+			var title = slide.Shapes[1].TextFrame.TextRange;
+			title.Text = sparrow.Item1.Split('\n').First();
+			title.Font.Name = "Arial";
+			title.Font.Size = 80;
+			var subtitle = slide.Shapes[2].TextFrame.TextRange;
+			subtitle.Text = sparrow.Item1.Split('\n').Last();
+			subtitle.Font.Name = "Arial";
+			subtitle.Font.Size = 30;
+			time = GetTimingsForAnswer(counter);
+			totalTime += time;
+			slide.SlideShowTransition.AdvanceTime = time;
+			slide.SlideShowTransition.AdvanceOnTime = MsoTriState.msoTrue;
+			return totalTime;
+		}
+
 		private static float GetTimingsForImage(int counter)
 		{
-			if (counter < 20)
+			if (counter < 5)
 			{
 				return 4.0f;
 			}
-			else if (counter < 30)
+			else if (counter < 12)
 			{
 				return 3.0f;
 			}
-			else if (counter < 40)
+			else if (counter < 20)
 			{
 				return 2.0f;
 			}
-			else if (counter < 60)
+			else if (counter < 30)
 			{
 				return 1.5f;
 			}
@@ -109,7 +127,23 @@ namespace PowerPointGeneration.Tests
 
 		private static float GetTimingsForAnswer(int counter)
 		{
-			return counter < 20 ? 1.5f : 0.75f;
+			if (counter < 5)
+			{
+				return 1.5f;
+			}
+			else if (counter < 12)
+			{
+				return 1;
+			}
+			else if (counter < 20)
+			{
+				return 0.75f;
+			}
+			else if (counter < 30)
+			{
+				return 0.5f;
+			}
+			return 0.5f;
 		}
 	}
 }
