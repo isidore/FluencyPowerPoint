ba0f702236444399acec568132d2f93e0b799a5d
Llewellyn Falco <isidore@setgame.com>
2016-03-25 18:20:52 -0700

    More Images

--- PowerPointGeneration/PowerPointGeneration.Tests/GeneratePowerpoint.cs
+++ PowerPointGeneration/PowerPointGeneration.Tests/GeneratePowerpoint.cs
@@ -1,5 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using ApprovalUtilities.SimpleLogger;
+﻿using ApprovalUtilities.SimpleLogger;
 using ApprovalUtilities.SimpleLogger.Writers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -60,22 +59,52 @@ namespace PowerPointGeneration.Tests
         [TestMethod]
         public void CreateSlidesForCodeSmells()
         {
-//            Logger.Writer = new ConsoleWriter();
-//             
-//            CodeSmells.Create(new Details()
-//            {
-//                Name = "LongMethods",
-//                GoodName = "Short Enough",
-//                GoodCount = 18,
-//                BadName = "Too Long",
-//                BadCount = 17,
-//                BackgroundColor = 0x272822, 
-//                FontSize = 90
-//            });
-            CodeSmells.Create(new Details() {Name = "BadNames", GoodName = "Good", GoodCount = 10, BadName = "Bad", BadCount = 29});
-//            CodeSmells.Create(new Details() {Name = "Clutter", GoodName = "Relevant", GoodCount = 7, BadName = "Clutter", BadCount = 28});
-//            CodeSmells.Create(new Details() {Name = "Duplication", GoodName = "Distinct", GoodCount = 4, BadName = "Duplication", BadCount = 7, FontSize = 100});
-//            CodeSmells.Create(new Details() { Name = "Inconsistency", GoodName = "Consistency", GoodCount = 1, BadName = "Inconsistency", BadCount =4,   FontSize = 100});
+            Logger.Writer = new ConsoleWriter();
+
+            CodeSmells.Create(new Details()
+            {
+                Name = "LongMethods",
+                GoodName = "Short Enough",
+                GoodCount = 18,
+                BadName = "Too Long",
+                BadCount = 17,
+                BackgroundColor = 0x272822,
+                FontSize = 90
+            });
+            CodeSmells.Create(new Details()
+            {
+                Name = "BadNames",
+                GoodName = "Good",
+                GoodCount = 10,
+                BadName = "Bad",
+                BadCount = 29
+            });
+            CodeSmells.Create(new Details()
+            {
+                Name = "Clutter",
+                GoodName = "Relevant",
+                GoodCount = 28,
+                BadName = "Clutter",
+                BadCount = 28
+            });
+            CodeSmells.Create(new Details()
+            {
+                Name = "Duplication",
+                GoodName = "Distinct",
+                GoodCount = 4,
+                BadName = "Duplication",
+                BadCount = 7,
+                FontSize = 100
+            });
+            CodeSmells.Create(new Details()
+            {
+                Name = "Inconsistency",
+                GoodName = "Consistency",
+                GoodCount = 1,
+                BadName = "Inconsistency",
+                BadCount = 4,
+                FontSize = 100
+            });
         }
 
         [TestMethod]
